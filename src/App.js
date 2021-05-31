c4dc074cb9160d901cdce9b48b6bcbe8b1153af9
Akarsh Shrivastava <82137709+AkarshShrivastava2@users.noreply.github.com>
2021-05-31 16:02:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import './styles.css';
 
+const people = [
+  "Instagram",
+  "Youtube",
+  "Pexel",
+  "Facebook",
+  "Twitter",
+  "Linkedin",
+  "Tiktok"
+];
 function App() {
+  const [search, setSearch] = React.useState("");
+  const [searchResults, setSearchResults] = React.useState([]);
+  const handleChange = e => {
+    setSearch(e.target.value);
+  };
+  React.useEffect(() => {
+    const results = people.filter(person =>
+      person.toLowerCase().includes(search)
+    );
+    setSearchResults(results);
+  }, [search]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        placeholder="Search From"
+        value={search}
+        onChange={handleChange}
+      />
+      <ul>
+        {searchResults.map(item => (
+          <li>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
